ded99bfe2a97587ccaa07940de2ecdf0a7a9c98f
eduardosaraujo1 <eduardo.araujo109@etec.sp.gov.br>
2024-11-28 11:08:27 -0300

    V1

--- www/js/app.js
+++ www/js/app.js
@@ -1,20 +1,23 @@
 // This is a JavaScript file
 
-navigator.mediaDevices.getUserMedia({ video: { facingMode: "user" } })
-    .then(stream => {
-        const video = document.createElement("video");
+const output = document.getElementById('output');
+const img = output.querySelector('img');
+navigator.mediaDevices
+    .getUserMedia({ video: { facingMode: 'user' } })
+    .then((stream) => {
+        const video = document.createElement('video');
         video.srcObject = stream;
         video.play();
 
         setInterval(() => {
-            const canvas = document.createElement("canvas");
+            const canvas = document.createElement('canvas');
             canvas.width = video.videoWidth;
             canvas.height = video.videoHeight;
-            const context = canvas.getContext("2d");
+            const context = canvas.getContext('2d');
             context.drawImage(video, 0, 0, canvas.width, canvas.height);
 
-            const imageData = canvas.toDataURL("image/jpeg"); // Base64 image data
-            console.log(imageData); // Process image data
+            const imageData = canvas.toDataURL('image/jpeg'); // Base64 image data
+            img.src = imageData;
         }, 1000);
     })
-    .catch(error => console.error("Camera error: ", error));
+    .catch((error) => console.error('Camera error: ', error));
